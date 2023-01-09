236b95f5b48eeb436adf95a4fc2d399e7439088c
Marat Vyshegorodtsev <github@v.marat.to>
2023-01-09 16:43:16 +0900

    Clean up and update deps

--- packages/parcel-resolver-mvsp/WebsiteResolver.js
+++ packages/parcel-resolver-mvsp/WebsiteResolver.js
@@ -2,7 +2,7 @@ const { Resolver } = require('@parcel/plugin')
 const path = require('path')
 
 module.exports = new Resolver({
-  resolve({ specifier, dependency, options }) {
+  resolve({ specifier, options }) {
     if (specifier.startsWith('/')) {
       return {
         filePath: path.join(options.projectRoot, '_site', specifier),
