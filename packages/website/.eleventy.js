48abc4874ddeb2f9aa8a1bd38889263c02327368
Marat Vyshegorodtsev <github@v.marat.to>
2023-07-31 13:53:46 +0900

    Update versions, cleanup repo structure

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -1,15 +1,17 @@
 const asciidoctor = require('asciidoctor')()
+const eleventyParcelPlugin = require('@kitschpatrol/eleventy-plugin-parcel')
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyParcelPlugin)
   eleventyConfig.addWatchTarget('./tailwind.config.js')
   eleventyConfig.addWatchTarget('./src/**/*.*')
   eleventyConfig.addPassthroughCopy('./src/robots.txt')
   eleventyConfig.addPassthroughCopy('./src/fonts/')
   eleventyConfig.addPassthroughCopy('./src/images/')
   eleventyConfig.addPassthroughCopy('./src/styles/')
-  eleventyConfig.addShortcode("asciidoc", function(value) {
+  eleventyConfig.addShortcode('asciidoc', function (value) {
     return `${asciidoctor.convert(value)}`
-  });
+  })
   return {
     templateFormats: ['html', 'njk', '11ty.js', 'md'],
     htmlTemplateEngine: 'njk',
