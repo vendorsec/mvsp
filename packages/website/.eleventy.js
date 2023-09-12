53f201f23e4ba8b9841466311904db3a6751e4a0
Chris John Riley <reg@c22.cc>
2023-09-12 19:00:16 +0200

    Update .eleventy.js

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/robots.txt')
   eleventyConfig.addPassthroughCopy('./src/fonts/')
   eleventyConfig.addPassthroughCopy('./src/images/')
-  eleventyConfig.addPassthroughCopy('./src/images/contributors')
+  eleventyConfig.addPassthroughCopy('./src/images/contributors/')
   eleventyConfig.addPassthroughCopy('./src/styles/')
   eleventyConfig.addShortcode('asciidoc', function (value) {
     return `${asciidoctor.convert(value)}`
