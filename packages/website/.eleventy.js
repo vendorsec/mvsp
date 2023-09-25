51f055cc884f9e4591a7f65851635808671ea3b5
Chris John Riley <reg@c22.cc>
2023-09-25 13:57:07 +0000

    Confirm if configured attributes are rendered

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/styles/')
   eleventyConfig.addPassthroughCopy('./src/js/')
   eleventyConfig.addShortcode('asciidoc', function (value) {
-    return `${asciidoctor.convertFile(value)}`
+    return `${asciidoctor.convert(value)}`
   })
   return {
     templateFormats: ['html', 'njk', '11ty.js', 'md'],
